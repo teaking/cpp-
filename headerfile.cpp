53951b0edf4fcef572cbb12aa1278d5c90b33f05
kim <4work.aber@gmail.com>
2018-02-28 22:30:00 +0000

    header (.hpp) can be used

---
+++ headerfile.cpp
@@ -0,0 +1,10 @@
+//using header file, main.hpp to keep code clean
+//all the functions can be hidden in hpp file which can create
+//abstractions
+#include "main.hpp"
+
+int main(){
+  cout<<"Hello, I use header files."<<endl;
+  return 0;
+
+}
